d0dc8c05cc77ea11dd636291d0b55b05c2805942
Manuel <manuelcolorado19@gmail.com>
2017-03-03 10:24:02 -0500

    farmaceuta en recipe

---
+++ resources/views/historia/show.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+    <div class="container">
+        @if(session('mensaje'))
+            <div class="row">
+                <div class="col md-12">
+                    <div class="alert alert-info alert-dismissible" role="info">
+                        <button type="button" class="close" data-dismiss="info" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <strong>Aviso:</strong> {{session ('mensaje')}}.
+                    </div>
+                </div>
+
+            </div>
+        @endif
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Medicos</div>
+
+                    <div class="panel-body">
+                        Listado de Historias
+                  {{--@if(Auth::user()->roles[0]->hasPermissionTo('CrearRole') or Auth::user()->can('CrearRole'))
+
+                       @endif--}}
+
+                            <a href="{{url('historias/create')}}" class="btn btn-success">
+                                <i class="fa fa-user"></i> Nueva Historia
+
+
+
+
+{{--comentario en laravel  <a href="{{url('usuarios/create')}}" class="btn btn-success">
+                            <i class="fa fa-user"></i> Nuevo Role
+POST REDIRECCIONA PORQUE EJECUTA LA MISMA ACCION--}}
+                        </a>
+
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Paciente</th>
+                                <th>Doctor</th>
+                                <th>Informe</th>
+
+
+                                <th colspan="3" width="10%">Acciones</th>
+                            </tr>
+{{--Auth::user()->hasRole('Asministrador')
+
+Auth::user('Administrador') as $user--}}
+                            @foreach($historias as $usuario)
+                                <tr>
+                                    <td>{{$usuario->user->nombre." ".$usuario->user->apellido}}</td>
+                                    <td>{{$usuario->doctores->nombre." ".$usuario->doctores->apellido}}</td>
+                                    <td>{{$usuario->informe}}</td>
+                                    <td>
+                                    @foreach($usuario->cita() as $cita)
+                                    <td>{{$cita->medicina_1}}</td>
+                                    @endforeach
+                                    </td>
+
+
+
+                                        <td><a href="{{url('/historias/'.$usuario->id)}}" class="btn btn-primary">
+                                                <!--i.glaphicon.glaphicon-edit-->
+                                                <i class="fa fa-edit"></i>
+                                            </a>
+
+                                        </td>
+                                    <td><a href="{{url('/historias/'.$usuario->id.'/edit')}}" class="btn btn-primary">
+                                            <!--i.glaphicon.glaphicon-edit-->
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-danger"
+                                                data-action="{{ url('/historias/'.$usuario->id) }}"
+                                                data-name="{{ $usuario->user->nombre." ".$usuario->user->apellido}}"
+                                                data-toggle="modal" data-target="#confirm-delete">
+                                            <i class ="fa fa-trash fa 1x"></i>
+                                        </button>
+                                    </td>
+
+
+                                </tr>
+
+
+                            @endforeach
+                        {{-- <th colspan="4" class="text-center">{{$usuarios->links()}}</th>--}}
+                        </table>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="confirm-delete" tabindex="-1"
+         role="dialog" aria-labelledby="myModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+                    <p>¿Seguro que desea eliminar este
+                        registro?</p>
+                    <p class="nombre"></p>
+                </div>
+                <div class="modal-footer">
+                    <form class="form-inline form-delete"
+                          role="form"
+                          method="POST"
+                          action="">
+                        {!! method_field('DELETE') !!}
+                        {!! csrf_field() !!}
+
+                        <button type="button"
+                                class="btn btn-default"
+                                data-dismiss="modal">Cancelar
+                        </button>
+
+                   {{-- @if(Auth::user()->roles[0]->hasPermissionTo('EliminarUsuario') or Auth::user()->can('EliminarUsuario'))--}}
+                        <button id="delete-btn"
+                                class="btn btn-danger"
+                                title="Eliminar">Eliminar
+                        </button>
+                    {{-- @endif--}}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,214 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Crear Usuario</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/usuarios') }}">
+                            {{ csrf_field('POST')}}
+                            {{ csrf_field()}}
+
+                            <div class="form-group{{ $errors->has('nombre') ? ' has-error' : '' }}">
+                                <label for="nombre" class="col-md-4 control-label">Nombre</label>
+
+                                <div class="col-md-6">
+                                    <input id="nombre" type="text" class="form-control" name="nombre" value="{{ old('nombre') }}" required autofocus>
+
+                                    @if ($errors->has('nombre'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('nombre') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('apellido') ? ' has-error' : '' }}">
+                                <label for="name" class="col-md-4 control-label">Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="apellido" type="text" class="form-control" name="apellido" value="{{ old('apellido') }}" required autofocus>
+
+                                    @if ($errors->has('apellido'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('apellido') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('cedula') ? ' has-error' : '' }}">
+                                <label for="cedula" class="col-md-4 control-label">Cedula</label>
+
+                                <div class="col-md-6">
+                                    <input id="cedula" type="text" class="form-control" name="cedula" value="{{ old('cedula') }}" required autofocus>
+
+                                    @if ($errors->has('cedula'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('cedula') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('fecha_nac') ? ' has-error' : '' }}">
+                                <label for="fecha_nac" class="col-md-4 control-label">Edad</label>
+
+                                <div class="col-md-6">
+                                    <input id="fecha_nac" type="text" class="form-control" name="fecha_nac" value="{{ old('fecha_nac') }}" required autofocus>
+
+                                    @if ($errors->has('fecha_nac'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('fecha_nac') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('direccion') ? ' has-error' : '' }}">
+                                <label for="direccion" class="col-md-4 control-label">Direccion</label>
+
+                                <div class="col-md-6">
+                                    <input id="direccion" type="text" class="form-control" name="direccion" value="{{ old('direccion') }}" required autofocus>
+
+                                    @if ($errors->has('direccion'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('direccion') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
+                                <label for="telefono" class="col-md-4 control-label">Telefono</label>
+
+                                <div class="col-md-6">
+                                    <input id="telefono" type="text" class="form-control" name="telefono" value="{{ old('telefono') }}" required autofocus>
+
+                                    @if ($errors->has('telefono'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('telefono') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('celular') ? ' has-error' : '' }}">
+                                <label for="celular" class="col-md-4 control-label">Celular</label>
+
+                                <div class="col-md-6">
+                                    <input id="celular" type="text" class="form-control" name="celular" value="{{ old('celular') }}" required autofocus>
+
+                                    @if ($errors->has('celular'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('celular') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('sexo') ? ' has-error' : '' }}">
+                                <label for="sexo" class="col-md-4 control-label">Sexo</label>
+                                <label class="radio-inline" style="margin-left:2%;"><input type="radio" name="sexo" value="femenino">F</label>
+                                <label class="radio-inline"><input type="radio" name="sexo" value="masculino">M</label>
+                                @if ($errors->has('sexo'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('sexo') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+                            <div class="form-group">
+                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
+                            </div>
+
+
+                                <div class="form-group{{$errors->has('role') ? 'has-error' : ''}}">
+                                    <label for="role" class="col-md-4 control-label">Rol</label>
+                                    <div class="col-md-6">
+                                        <select name="role" id="role" class="form-control" >
+
+                                            @foreach($roles as $role)
+                                                <option value="{{$role->name}}">{{$role->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @if($errors->has('role'))
+                                            <span class="help-block"></span>
+                                            <strong>{{$errors->first('role')}}</strong>
+                                        @endif
+                                    </div>
+                                </div>
+
+                            <div class="form-group{{$errors->has('espe') ? 'has-error' : ''}}">
+                                <label for="especialidad" class="col-md-4 control-label">Especialidad</label>
+                                <div class="col-md-6">
+                                    <select name="especialidad" id="especialidad" class="form-control" >
+                                        <option value="4" selected>Seleccione en caso de ser medico</option>
+                                        @foreach($espe->slice(0,3) as $espe)
+
+                                            <option value="{{$espe->id}}" @if($espe->descrpcion=="n/a") selected @endif>{{$espe->descripcion}}</option>
+                                        @endforeach
+                                    </select>
+                                    @if($errors->has('especialidad'))
+                                        <span class="help-block"></span>
+                                        <strong>{{$errors->first('especialidad')}}</strong>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <!--
+                            Cita
+                            especialidad
+                            medico
+                            fecha
+                            paciente
+                            estatus
+                            ($user->role('medico') as $medico)
+                            -->
+
+
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Register
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
